d46d4bcb3a2fd27d61e2e3836a6f2e1faee257b2
Piyush Pamnani <piyushpamnani46@gmail.com>
2023-03-03 00:31:58 +0530

    Correct Name after Downloading Resume

--- src/Components/header/Download_Buttons.jsx
+++ src/Components/header/Download_Buttons.jsx
@@ -4,7 +4,7 @@ import CV from "../../assests/Piyush_Pamnani.pdf";
 const Download_Buttons = () => {
   return (
     <div className="download_btns">
-      <a href={CV} download className="btn">
+      <a href={CV} download="Piyush_Pamnani.pdf" className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
