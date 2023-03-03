514b61760ae125969d066118ee1d014b535c3df6
Piyush Pamnani <piyushpamnani46@gmail.com>
2023-03-03 13:51:21 +0530

    SEO

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
+import React from "react";
 import ReactDOM from "react-dom";
+import { Helmet } from "react-helmet";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const title = "Piyush Pamnani";
+const description = "Welcome to Piyush's Portfolio.";
+const keywords = "Piyush Pamnani, Piyush, Pamnani, Portfolio";
+const author = "Piyush Pamnani";
+const canonical = "https://piyushpamnani.github.io/Piyush-Portfolio";
+
+ReactDOM.render(
+  <>
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <meta name="author" content={author} />
+      <link rel="canonical" href={canonical} />
+    </Helmet>
+    <App />
+  </>,
+  document.querySelector("#root")
+);
