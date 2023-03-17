e5773e9abeb4d0e1007c1c8551df02191733a7a8
Piyush Pamnani <piyushpamnani46@gmail.com>
2023-03-17 12:42:54 +0530

    Experience Description

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import { React, useState } from "react";
+import { IconContext } from "react-icons";
 import { BsPatchCheckFill } from "react-icons/bs";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 import "./experience.css";
 
 const Experience = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleModel = () => setIsActive(!isActive);
   return (
     <section id="experience">
       <h5>Journey So far</h5>
@@ -30,6 +35,42 @@ const Experience = () => {
               <BsPatchCheckFill className="experience_details-icon" />
               <h4>Flask</h4>
             </article>
+          </div>
+          <span className="experience_details-more">
+            <span onClick={toggleModel}>View More</span>
+          </span>
+          <div className={`experience_model ${isActive ? "active-model" : ""}`}>
+            <div class="experience_model-content">
+              <h4 class="experience_model-title">
+                Noted <br /> Accomplishments
+              </h4>
+              <i class="experience_model-close" onClick={toggleModel}>
+                <AiOutlineCloseCircle />
+              </i>
+
+              <ul class="experience_model-points">
+                <IconContext.Provider value={{ size: "1em" }}>
+                  <li class="experience_model-point">
+                    <BsPatchCheckFill className="experience_details-icon" />
+                    <p>Edited and added new features to an existing website.</p>
+                  </li>
+                  <li class="experience_model-point">
+                    <BsPatchCheckFill className="experience_details-icon" />
+                    <p>
+                      Improved the website's navigation bar and user interface.
+                    </p>
+                  </li>
+                  <li class="experience_model-point">
+                    <BsPatchCheckFill className="experience_details-icon" />
+                    <p>Dynamic QR code generation with unique runtime URLs.</p>
+                  </li>
+                  <li class="experience_model-point">
+                    <BsPatchCheckFill className="experience_details-icon" />
+                    <p>Added a download button for downloading a CSV file.</p>
+                  </li>
+                </IconContext.Provider>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
