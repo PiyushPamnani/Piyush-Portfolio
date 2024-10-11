c7be2d24a55cd42f8db6389dc74ba0b270682d1d
Piyush Pamnani <piyushpamnani46@gmail.com>
2024-10-11 14:40:36 +0530

    SEO fixes

--- src/Components/header/SocialMediaLinks.jsx
+++ src/Components/header/SocialMediaLinks.jsx
@@ -1,21 +1,20 @@
 import React from "react";
-import { BsLinkedin, BsGithub } from "react-icons/bs";
-import { SiLeetcode } from "react-icons/si";
+import { BsLinkedin, BsGithub, BsStackOverflow } from "react-icons/bs";
 
 const SocialMediaLinks = () => {
   return (
     <div className="social_links">
-      <a
-        href="https://www.linkedin.com/in/piyushpamnani2001/"
-        target="_blank"
-      >
+      <a href="https://www.linkedin.com/in/piyushpamnani2001/" target="_blank">
         <BsLinkedin />
       </a>
       <a href="https://github.com/PiyushPamnani" target="_blank">
         <BsGithub />
       </a>
-      <a href="https://leetcode.com/piyushpamnani22/" target="_blank">
-        <SiLeetcode />
+      <a
+        href="https://stackoverflow.com/users/27458191/piyush-pamnani"
+        target="_blank"
+      >
+        <BsStackOverflow />
       </a>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,30 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import { Helmet } from "react-helmet";
 import App from "./App";
 import "./index.css";
 
-const title = "Piyush Pamnani";
-const description = "Welcome to Piyush's Portfolio.";
-const keywords =
-  "Piyush Pamnani, Piyush, Pamnani, piyush, pamnani, piyush pamnani";
-const author = "Piyush Pamnani";
-
 const root = ReactDom.createRoot(document.getElementById("root"));
 
-root.render(
-  <>
-    <Helmet>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="description" content={description} />
-      <meta name="keywords" content={keywords} />
-      <meta name="author" content={author} />
-      <link
-        rel="canonical"
-        href="https://piyushpamnani.github.io/Piyush-Portfolio"
-      />
-    </Helmet>
-    <App />
-  </>
-);
+root.render(<App />);
